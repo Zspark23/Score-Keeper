ea23b77e9ba990ede814d93a0b9d737f4c87e5d8
Zack Spicer <spicer.zack8@gmail.com>
2015-07-06 19:02:35 -0600

    Set the root view controller

--- Score Keeper/SKAppDelegate.m
+++ Score Keeper/SKAppDelegate.m
@@ -7,13 +7,16 @@
 //
 
 #import "SKAppDelegate.h"
+#import "ScoreViewController.h"
 
 @implementation SKAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[ScoreViewController new]];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
