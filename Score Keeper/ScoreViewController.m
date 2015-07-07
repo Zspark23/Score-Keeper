c15283f6015667a83d5fd19ef567d589e59916fc
Zack Spicer <spicer.zack8@gmail.com>
2015-07-06 19:00:07 -0600

    Added ScoreViewController

---
+++ Score Keeper/ScoreViewController.m
@@ -0,0 +1,28 @@
+//
+//  ScoreViewController.m
+//  Score Keeper
+//
+//  Created by Zack Spicer on 7/6/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "ScoreViewController.h"
+
+@interface ScoreViewController ()
+
+@end
+
+@implementation ScoreViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
